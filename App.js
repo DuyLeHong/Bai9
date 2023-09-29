de8cafe7ea7b5e1f824134df8ca48bfbae3fad71
Duy Le <lehongduy.bk@gmail.com>
2023-09-29 17:52:26 +0700

    update code

--- App.js
+++ App.js
@@ -2,14 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import WebView from 'react-native-webview';
 import MyWebView from './MyWebView';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('vao useEffect')
+      setLoading(false)
+    }, 6000)
+  }, [])
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
         
         <MyWebView />
 
+        <ActivityIndicator animating = {isLoading}/>
       </View>
     </SafeAreaView>
   );
